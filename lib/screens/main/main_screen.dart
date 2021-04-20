4b2b3ee5f36b08145813f3b2fbd770ef908a6d80
sibongiseni <sibongiseni.ndebele@idolconsulting.co.za>
2021-04-20 10:52:28 +0200

    setup

---
+++ lib/screens/main/main_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 400,
+            color: kDarkBlackColor,
+          )
+        ],
+      ),
+    );
+  }
+}
